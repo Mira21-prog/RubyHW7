a9c2de2439d58542b39a987986161d4eb32b411a
Mira21-prog <matsakovamira156@gmail.com>
2020-12-18 20:43:37 +0200

    add foter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :image, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
   validates :title, presence: true
   validates :content, presence: true
