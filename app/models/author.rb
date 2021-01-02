13c4f220370fbc82d16e2bef907a6d258decab7a
Mira <matsakovamira156@gmail.com>
2021-01-02 13:45:11 +0200

    add fix with sign up form

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,12 +4,16 @@ class Author < ApplicationRecord
   has_one_attached :avatar
 
   before_save { email.downcase! }
+  validates :gender, presence: true
+  validates :birthday, presence: true
+  validates :avatar, presence: true
   validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { minimum: 8},
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+
 
   def full_name
     "#{first_name} #{last_name}" if first_name.present? && last_name.present?
